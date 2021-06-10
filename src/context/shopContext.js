512370380bc4129ff5b184fc159f24513616d723
Patrick Roth <patrockdev@icloud.com>
2021-06-10 13:47:35 +0200

    REACT_APP_SHOPIFY_DOMAIN

--- src/context/shopContext.js
+++ src/context/shopContext.js
@@ -1,72 +1,85 @@
-import React, { Component } from 'react'
-import Client from 'shopify-buy';
+import React, { Component } from "react";
+import Client from "shopify-buy";
 
 const ShopContext = React.createContext();
 
 const client = Client.buildClient({
-    domain: process.env.REACT_APP_DOMAIN,
-    storefrontAccessToken: process.env.REACT_APP_SHOPIFY_API
-  });
+  domain: process.env.REACT_APP_SHOPIFY_DOMAIN,
+  storefrontAccessToken: process.env.REACT_APP_SHOPIFY_API,
+});
 
 class ShopProvider extends Component {
+  state = {
+    product: {},
+    products: [],
+    checkout: {},
+    isCartOpen: false,
+    isMenuOpen: false,
+  };
 
-    state = {
-        product: {},
-        products: [],
-        checkout: {},
-        isCartOpen: false,
-        isMenuOpen: false
+  componentDidMount() {
+    if (localStorage.checkout_id) {
+        this.fetchCheckout(localStorage.checkout_id)
+    } else {
+      this.createCheckout()
     }
+  }
 
-    componentDidMount() {
-        this.createCheckout()
-    }
+  createCheckout = async () => {
+    const checkout = await client.checkout.create();
+    localStorage.setItem("checkout_id", checkout.id);
+    this.setState({ checkout: checkout });
+  }
 
-    createCheckout = async () => {
-        const checkout = await client.checkout.create();
-        this.setState({ checkout: checkout })
-    }
-    
-    fetchCheckout = async () => {
+  fetchCheckout = (checkoutId) => {
+      client.checkout
+      .fetch(checkoutId)
+      .then((checkout) => {
+          this.setState({ checkout: checkout })
+      })
+  }
 
-    }
+  addItemtoCheckout = async () => {};
 
-    addItemtoCheckout = async () => {
+  removeLineItem = async (lineItemIdsToRemove) => {};
 
-    }
+  fetchAllProducts = async () => {
+    const products = await client.product.fetchAll();
+    this.setState({ products: products });
+  }
 
-    removeLineItem = async (lineItemIdsToRemove) => {
+  fetchProductWithHandle = async (handle) => {
+    const product = await client.product.fetchByHandle(handle);
+    this.setState({ product: product });
+  };
 
-    }
+  closeCart = () => {};
 
-    fetchAllProducts = async () => {
-        const products = await client.product.fetchAll();
-        this.setState({ products: products })
-    }
+  openCart = () => {};
 
-    fetchProductWithHandle = async (handle) => {
-       const product = await client.product.fetchByHandle(handle)
-       this.setState({ product: product })
-    }
+  closeMenu = () => {};
 
-    closeCart = () => {}
+  openMenu = () => {};
 
-    openCart = () => {}
-
-    closeMenu = () => {}
-
-    openMenu = () => {}
-
-    render() {
-
-    console.log(this.state.checkout)
-
-        return (
-            <ShopContext.Provider>
-               {this.props.children} 
-            </ShopContext.Provider>
-        )
-    }
+  render() {
+    return (
+        <ShopContext.Provider 
+        value={{ 
+        ...this.state,
+        fetchAllProducts: this.fetchAllProducts,
+        fetchProductWithHandle: this.fetchProductWithHandle,
+        addItemtoCheckout: this.addItemtoCheckout,
+        removeLineItem: this.removeLineItem, 
+        closeCart: this.closeCart,
+        openCart: this.openCart,
+        closeMenu: this.closeMenu,
+        openMenu: this.openMenu,
+  
+    }}>
+    {this.props.children}
+    </ShopContext.Provider>
+    );
+  }
 }
 
 const ShopConsumer = ShopContext.Consumer;
@@ -74,4 +87,3 @@ const ShopConsumer = ShopContext.Consumer;
 export { ShopConsumer, ShopContext };
 
 export default ShopProvider;
-
---
+++ src/pages/Home.js
@@ -0,0 +1,26 @@
+import React, { useContext, useEffect } from 'react'
+
+import { ShopContext } from '../context/shopContext'
+
+const Home = () => {
+
+    const { fetchAllProducts, products } = useContext(ShopContext)
+
+    useEffect(() => {
+        fetchAllProducts()
+    }, [fetchAllProducts])
+
+    if(!products) return <div>Loadings...</div>
+    
+    return (
+        <div>
+            {
+                products.map(product => (
+                    <h1>{product.title}</h1>
+                ))
+            }
+        </div>
+    )
+}
+
+export default Home
