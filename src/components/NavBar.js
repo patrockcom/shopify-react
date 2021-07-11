ee2ed50a02aac3c5cc4afdf4c4f487f79e273979
Patrick Roth <patrockdev@icloud.com>
2021-07-11 18:36:10 +0200

    Finishing Cart and Navigation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 
 import React, { useContext } from 'react'
-import { Flex, Text,Icon, Image } from '@chakra-ui/react'
+import { Flex, Box, Icon, Image, Badge } from '@chakra-ui/react'
 import { ShopContext } from '../context/shopContext'
 import { Link } from 'react-router-dom'
 import { MdMenu, MdShoppingBasket } from "react-icons/md";
@@ -10,15 +10,19 @@ const NavBar = () => {
     const { openCart, openMenu, checkout } = useContext(ShopContext)
     
     return (
-        <Flex backgroundColor="#FFABE2" flexDir="row" justifyContent="space-between" p="2rem" >
+        <Flex backgroundColor="#FFABE2" flexDir="row" alignItems="center" justifyContent="space-between" p="2rem" >
             <Icon fill="white" cursor="pointer" as={MdMenu} w={30} h={30}
            onClick={() => openMenu()}
            ></Icon>
            <Link to="/"><Image src="https://cdn.shopify.com/s/files/1/0472/5705/9496/files/Logologo_1.svg?v=1610055540" 
             w={100} h={100} /></Link>
-            <Icon fill="white" as={MdShoppingBasket} w={30} h={30} 
+            <Box>
+                <Icon fill="white" as={MdShoppingBasket} w={30} h={30} 
                 onClick={() => openCart()}
             />
+            <Badge backgroundColor="#FF38BD" borderRadius="50%">{checkout?.lineItems?.length}</Badge>        
+            </Box>
+            
         </Flex>
     )
 }
